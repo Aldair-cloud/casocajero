3d41f2e4ac975935d2b279f6f9011aa8908f91fb
Aldair-cloud <leonelsanchez@upeu.edu.pe>
2020-05-25 02:44:43 -0500

    1push

---
+++ js/script.js
@@ -0,0 +1,36 @@
+var pass = ''
+var clave = '1234'
+
+function ingresar(opcion){
+    switch(opcion){
+        case 1:
+            if(pass.length==4){
+                if(pass == clave){
+                    document.getElementById('pantalla1').style.display='none';
+                    document.getElementById('vistamenuprincipal').style.display='block';
+                }
+            }
+    }
+}
+
+function recibirboton(){
+
+}
+function okay(){
+
+    if(document.getElementById('pantalla1').style.display='none'){
+        document.getElementById('vistamenuprincipal').style.display='block';
+    }
+}
+function ingresoretiro(){
+    if(document.getElementById('vistamenuprincipal').style.display='none'){
+        document.getElementById('vistaretirar').style.display='block';
+        document.getElementById('retiro').style.display='none';
+        document.getElementById('retirosoles').style.display='block';
+    }
+}
+function ingresarretisoles(){
+    if(document.getElementById('vistaretirar').style.display='none'){
+        document.getElementById('vistasoles').style.display='block';
+    }
+}
